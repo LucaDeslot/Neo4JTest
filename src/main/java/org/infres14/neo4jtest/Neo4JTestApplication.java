c2f4dde3f76010485932215c36982bcfd5361fc0
lucadeslot <deslot.luca@gmail.com>
2024-02-16 09:15:40 +0100

    first commit

---
+++ src/main/java/org/infres14/neo4jtest/Neo4JTestApplication.java
@@ -0,0 +1,13 @@
+package org.infres14.neo4jtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4JTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4JTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/infres14/neo4jtest/Neo4JTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.infres14.neo4jtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4JTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
